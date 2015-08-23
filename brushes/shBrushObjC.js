901dbb024791793e41cecb68a13cdc07ac8a487d
Jody Hagins <jodyhagins@users.noreply.github.com>
2015-08-23 09:49:30 -0400

    Update shBrushObjC.js

--- brushes/shBrushObjC.js
+++ brushes/shBrushObjC.js
@@ -7,39 +7,39 @@
 */
 ;(function()
 {
-    // CommonJS
-    typeof(require) != 'undefined' ? SyntaxHighlighter = require('shCore').SyntaxHighlighter : null;
+  // CommonJS
+  typeof(require) != 'undefined' ? SyntaxHighlighter = require('shCore').SyntaxHighlighter : null;
  
-    function Brush()
-    {
-		  // Copyright 2006 Shin, YoungJin
-		  // Copyright 2015 Jody Hagins
-		  var datatypes =	'ATOM BOOL BOOLEAN BYTE CHAR COLORREF DWORD DWORDLONG DWORD_PTR ' +
-		                  'DWORD32 DWORD64 FLOAT HACCEL HALF_PTR HANDLE HBITMAP HBRUSH ' +
-		                  'HCOLORSPACE HCONV HCONVLIST HCURSOR HDC HDDEDATA HDESK HDROP HDWP ' +
-		                  'HENHMETAFILE HFILE HFONT HGDIOBJ HGLOBAL HHOOK HICON HINSTANCE HKEY ' +
-		                  'HKL HLOCAL HMENU HMETAFILE HMODULE HMONITOR HPALETTE HPEN HRESULT ' +
-		                  'HRGN HRSRC HSZ HWINSTA HWND INT INT_PTR INT32 INT64 LANGID LCID LCTYPE ' +
-		                  'LGRPID LONG LONGLONG LONG_PTR LONG32 LONG64 LPARAM LPBOOL LPBYTE LPCOLORREF ' +
-		                  'LPCSTR LPCTSTR LPCVOID LPCWSTR LPDWORD LPHANDLE LPINT LPLONG LPSTR LPTSTR ' +
-		                  'LPVOID LPWORD LPWSTR LRESULT PBOOL PBOOLEAN PBYTE PCHAR PCSTR PCTSTR PCWSTR ' +
-		                  'PDWORDLONG PDWORD_PTR PDWORD32 PDWORD64 PFLOAT PHALF_PTR PHANDLE PHKEY PINT ' +
-		                  'PINT_PTR PINT32 PINT64 PLCID PLONG PLONGLONG PLONG_PTR PLONG32 PLONG64 POINTER_32 ' +
-		                  'POINTER_64 PSHORT PSIZE_T PSSIZE_T PSTR PTBYTE PTCHAR PTSTR PUCHAR PUHALF_PTR ' +
-		                  'PUINT PUINT_PTR PUINT32 PUINT64 PULONG PULONGLONG PULONG_PTR PULONG32 PULONG64 ' +
-		                  'PUSHORT PVOID PWCHAR PWORD PWSTR SC_HANDLE SC_LOCK SERVICE_STATUS_HANDLE SHORT ' +
-		                  'SIZE_T SSIZE_T TBYTE TCHAR UCHAR UHALF_PTR UINT UINT_PTR UINT32 UINT64 ULONG ' +
-		                  'ULONGLONG ULONG_PTR ULONG32 ULONG64 USHORT USN VOID WCHAR WORD WPARAM WPARAM WPARAM ' +
-		                  'char char16_t char32_t bool short int __int32 __int64 __int8 __int16 long float ' +
-		                  'double __wchar_t clock_t _complex _dev_t _diskfree_t div_t ldiv_t _exception ' +
-		                  '_EXCEPTION_POINTERS FILE _finddata_t _finddatai64_t _wfinddata_t _wfinddatai64_t ' +
-		                  '__finddata64_t __wfinddata64_t _FPIEEE_RECORD fpos_t _HEAPINFO _HFILE lconv ' +
-		                  'int8_t int16_t int32_t int64_t ' +
-		                  'intptr_t jmp_buf mbstate_t _off_t _onexit_t _PNH ptrdiff_t ' +
-		                  '_purecall_handler sig_atomic_t signed size_t _stat __stat64 _stati64 terminate_function ' +
-		                  'time_t __time64_t _timeb __timeb64 tm uintptr_t unsigned _utimbuf ' +
-		                  'uint8_t uint16_t uint32_t uint64_t ' +
-		                  'va_list wchar_t wctrans_t wctype_t wint_t';
+  function Brush()
+  {
+    // Copyright 2006 Shin, YoungJin
+    // Copyright 2015 Jody Hagins
+    var datatypes = 'ATOM BOOL BOOLEAN BYTE CHAR COLORREF DWORD DWORDLONG DWORD_PTR ' +
+                    'DWORD32 DWORD64 FLOAT HACCEL HALF_PTR HANDLE HBITMAP HBRUSH ' +
+		                'HCOLORSPACE HCONV HCONVLIST HCURSOR HDC HDDEDATA HDESK HDROP HDWP ' +
+		                'HENHMETAFILE HFILE HFONT HGDIOBJ HGLOBAL HHOOK HICON HINSTANCE HKEY ' +
+		                'HKL HLOCAL HMENU HMETAFILE HMODULE HMONITOR HPALETTE HPEN HRESULT ' +
+		                'HRGN HRSRC HSZ HWINSTA HWND INT INT_PTR INT32 INT64 LANGID LCID LCTYPE ' +
+		                'LGRPID LONG LONGLONG LONG_PTR LONG32 LONG64 LPARAM LPBOOL LPBYTE LPCOLORREF ' +
+		                'LPCSTR LPCTSTR LPCVOID LPCWSTR LPDWORD LPHANDLE LPINT LPLONG LPSTR LPTSTR ' +
+		                'LPVOID LPWORD LPWSTR LRESULT PBOOL PBOOLEAN PBYTE PCHAR PCSTR PCTSTR PCWSTR ' +
+		                'PDWORDLONG PDWORD_PTR PDWORD32 PDWORD64 PFLOAT PHALF_PTR PHANDLE PHKEY PINT ' +
+		                'PINT_PTR PINT32 PINT64 PLCID PLONG PLONGLONG PLONG_PTR PLONG32 PLONG64 POINTER_32 ' +
+		                'POINTER_64 PSHORT PSIZE_T PSSIZE_T PSTR PTBYTE PTCHAR PTSTR PUCHAR PUHALF_PTR ' +
+		                'PUINT PUINT_PTR PUINT32 PUINT64 PULONG PULONGLONG PULONG_PTR PULONG32 PULONG64 ' +
+		                'PUSHORT PVOID PWCHAR PWORD PWSTR SC_HANDLE SC_LOCK SERVICE_STATUS_HANDLE SHORT ' +
+		                'SIZE_T SSIZE_T TBYTE TCHAR UCHAR UHALF_PTR UINT UINT_PTR UINT32 UINT64 ULONG ' +
+		                'ULONGLONG ULONG_PTR ULONG32 ULONG64 USHORT USN VOID WCHAR WORD WPARAM WPARAM WPARAM ' +
+		                'char char16_t char32_t bool short int __int32 __int64 __int8 __int16 long float ' +
+		                'double __wchar_t clock_t _complex _dev_t _diskfree_t div_t ldiv_t _exception ' +
+		                '_EXCEPTION_POINTERS FILE _finddata_t _finddatai64_t _wfinddata_t _wfinddatai64_t ' +
+		                '__finddata64_t __wfinddata64_t _FPIEEE_RECORD fpos_t _HEAPINFO _HFILE lconv ' +
+		                'int8_t int16_t int32_t int64_t ' +
+		                'intptr_t jmp_buf mbstate_t _off_t _onexit_t _PNH ptrdiff_t ' +
+		                '_purecall_handler sig_atomic_t signed size_t _stat __stat64 _stati64 terminate_function ' +
+		                'time_t __time64_t _timeb __timeb64 tm uintptr_t unsigned _utimbuf ' +
+		                'uint8_t uint16_t uint32_t uint64_t ' +
+		                'va_list wchar_t wctrans_t wctype_t wint_t';
 
 		  var keywords =	'alignas alignof auto break case catch class const constexpr decltype __finally __exception __try ' +
 		                  'const_cast continue private public protected __declspec ' +
@@ -76,11 +76,11 @@
 		    { regex: SyntaxHighlighter.regexLib.doubleQuotedString,	 css: 'string' },
 		    { regex: SyntaxHighlighter.regexLib.singleQuotedString,	 css: 'string' },
 		    { regex: /^ *#.*/gm,										                 css: 'preprocessor' },
-		    { regex: /@\\w+\\b/g,                                    css: 'color2 bold' }, // ObjC keyword
+		    { regex: new RegExp('@\\w+\\b', 'g'),                    css: 'color2 bold' }, // ObjC keyword
 		    { regex: /@/g,                                           css: 'string' },      // Most likely NSString
-		    { regex: /\.?\\b\\d+[a-z]?\\b/g,                         css: 'constants' },   // Number
-		    { regex: /\\b(NS|UI|CG)\\w+\\b/g,                        css: 'color3' },      // Cocoa Prefixes
-		    { regex: /\\b(WJH|_WJH|wjh|_wjh)\\w+\\b/g,               css: 'variable' },    // My Prefixes
+		    { regex: new RegExp('\.?\\b\\d+[a-z]?\\b', 'g'),         css: 'constants' },   // Number
+		    { regex: new RegExp('\\b(NS|UI|CG)\\w+\\b', 'g'),        css: 'color3' },      // Cocoa Prefixes
+		    { regex: new RegExp('\\b_?(WJH|wjh)\\w+\\b', 'g'),       css: 'variable' },    // My Prefixes
 
 		    { regex: new RegExp(this.getKeywords(datatypes), 'gm'),		css: 'color1 bold' },
 		    { regex: new RegExp(this.getKeywords(functions), 'gm'),		css: 'functions bold' },
